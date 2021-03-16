c8ee6b2685fba05568c448c37140f62f9b8cefaa
Semysudo <dpdls0603@naver.com>
2021-03-16 08:16:38 +0000

    #다트게임#실패율

--- main.py
+++ main.py
@@ -1,23 +1,5 @@
-def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    print(participant)
-    print(completion)
-    #참가자는 2명인데 c에 1명만 있을 때
-    for p,c in zip(participant, completion):
-      if p != c:
-        
-        return p
+def solution(N,stages):
+  result = {}
+  num = len(stages)
 
-
-#참가자중 c에 없을때 
-    return participant.pop()
-
-
-participant = ["marina", "josipa", "marina", "vinko", "filipa"]
-completion = ["josipa", "filipa", "marina","vinko"]
-s = solution(participant,completion)
-print(s)
-
-
-  
+  for stage in rna
