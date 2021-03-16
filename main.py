8e02034fbb15ca41732153e802793dc540ca67e0
Semysudo <dpdls0603@naver.com>
2021-03-16 08:25:29 +0000

    #다트게임#실패율

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-def solution(N,stages):
-  result = {}
-  num = len(stages)
+def solution(n):
+    number=''
+    answer=0
+    while n > 0:
+        div = n // 3
+        mod = n % 3
+        n = div
+        number += str(mod)
+    # 45 = 1200(3) = number[::-1]
+    # 0021(3) = ?(10)
+    for idx, num in enumerate(number[::-1]):
+        answer += int(num)*(3**idx)
+    return answer
 
-  for stage in rna
+n=45
+s = solution(n)
+print(s)
