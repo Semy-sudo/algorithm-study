2f5bcfd7cb8da6a74d42ee1da58524dd7310942b
Semysudo <dpdls0603@naver.com>
2021-03-17 06:25:06 +0000

    #키패드누르기 #소수만들기

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-def solution(n):
-    number=''
-    answer=0
-    while n > 0:
-        div = n // 3
-        mod = n % 3
-        n = div
-        number += str(mod)
-    # 45 = 1200(3) = number[::-1]
-    # 0021(3) = ?(10)
-    for idx, num in enumerate(number[::-1]):
-        answer += int(num)*(3**idx)
+#키패드 누르기 소수 만들기
+from itertools import combinations
+
+def solution(nums):
+    answer = len(list(combinations(nums, 3)))
+    for l in list(combinations(nums, 3)):
+      for i in range(2,sum(l)):
+        if sum(l) % i == 0:
+          answer -=1
+          break
+      
     return answer
 
-n=45
-s = solution(n)
+
+
+nums = [1,2,3,4]
+s = solution(nums)
 print(s)
